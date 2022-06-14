d1113cc0b0a0202bda68f861ae668d5d6c5bf6bb
navarrettejatwit <46931515+navarrettejatwit@users.noreply.github.com>
2022-06-14 18:31:30 -0400

    Tower Spawner and Enemy Spawner Script Completed
    UI for Build Menu Pending ...

--- Assets/Scripts/MainGame/SpawnerScripts/TowerSpawner.cs
+++ Assets/Scripts/MainGame/SpawnerScripts/TowerSpawner.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class TowerSpawner : MonoBehaviour
 {
     //private List<Tower> allTowers = new List<Tower>();
-    
-    
 
-    [SerializeField] private Tower towerPrefab = null;
+    public Cell[,] VirtualSlotMatrix = null;
+
+    public GameObject SlotGrid = null;
+
+    public int row;
+    
+    public int column;
+
+    private SlotGrid sg;
+    
+    [SerializeField] private Tower TowerPrefab;
+
+    private float buildtime = 0;
+
+    private TowerFactory Tower_Factory;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        VirtualSlotMatrix = new Cell[row, column];
+        sg = SlotGrid.GetComponent<SlotGrid>();
+        sg.setRow(row);
+        sg.setColumn(column);
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                VirtualSlotMatrix[i, j] = new Cell(i, j, false);
+            }
+        }
+        Tower_Factory = new TowerFactory(TowerPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //To do if build tower buttons pressed and wait or not pressed.
         if (Input.GetMouseButtonDown(1))
         {
             RaycastHit hit;
@@ -27,8 +52,9 @@ public class TowerSpawner : MonoBehaviour
                 Transform objectHit = hit.transform;
                 int i = (int) objectHit.position.x;
                 int j = (int) objectHit.position.y;
-                //int newValue = placeTower(i, j);
-                //slotMatrix[i, j].setState(newValue);
+                bool fill = spawnTower(objectHit);
+                VirtualSlotMatrix[i, j].setIsFilled(fill);
+                //To do mark that tower was placed mouse input set disabled
             }
         }
 
@@ -41,10 +67,28 @@ public class TowerSpawner : MonoBehaviour
                 Transform objectHit = hit.transform;
                 int i = (int) objectHit.position.x;
                 int j = (int) objectHit.position.y;
-                //int newValue = sellTower(i, j);
-                //slotMatrix[i, j].setState(newValue);
+                bool notFilled = sellTower(objectHit);
+                VirtualSlotMatrix[i, j].setIsFilled(notFilled);
             }
         }
         
+    }
+
+    public bool spawnTower(Transform coord)
+    {
+        Tower_Factory.setSpawnPoint(coord);
+        Tower NewTower = (Tower) Tower_Factory.produce();
+        return true;
+    }
+
+    public bool sellTower(Transform coord)
+    {
+        //To Do//Tower Destroy Method Call; //During Restart All Towers Must Be Destroyed
+        return false;
+    }
+
+    public Cell[,] getVirtualSlotMatrix()
+    {
+        return VirtualSlotMatrix;
     }
 }
---
+++ Assets/Scripts/MainGame/TowerSlotGraphics/SlotGrid.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotGrid : MonoBehaviour
+{
+    
+    public GameObject SlotGraphic = null;
+
+    public GameObject Parent = null;
+    
+    public GameObject[,] SlotMatrix;
+    
+    private Cell[,] VirtualSlotMatrix;
+
+    private int row;
+    
+    private int column;
+
+    private TowerSpawner ts;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SlotMatrix = new GameObject[row, column];
+        ts = (TowerSpawner) GameObject.Find("TowerSpawner").GetComponent(typeof(TowerSpawner));
+        VirtualSlotMatrix = ts.getVirtualSlotMatrix();
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                Vector3 Position = new Vector3(i, j, 0);
+                SlotMatrix[i, j] = Instantiate(SlotGraphic, Position, Quaternion.identity, Parent.transform);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateSlotGraphic();
+    }
+
+    public void updateSlotGraphic()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                VirtualSlotMatrix[i, j] = ts.getVirtualSlotMatrix()[i, j];
+                //To Do// slot graphic
+            }
+        }
+    }
+
+    public void setRow(int r)
+    {
+        row = r;
+    }
+    
+    public void setColumn(int c)
+    {
+        
+        column = c;
+    }
+}
